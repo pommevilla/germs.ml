5fb4d0dd9cf7cf68ef81159b131c972d060b41f5
pommevilla <pvillanueva13@gmail.com>
2019-05-14 20:58:45 -0500

    Add plot_history function

--- src/gml_utils/visualization.py
+++ src/gml_utils/visualization.py
@@ -133,3 +133,29 @@ def show_image_with_predictions(i, predictions, true_label, img, label_names):
     plt.subplot(1, 2, 2)
     plot_predictions(i, predictions, true_label, label_names)
     plt.show()
+
+
+
+def plot_history(history):
+    """
+    Adapted from: https://realpython.com/python-keras-text-classification/
+    """
+    import matplotlib.pyplot as plt
+    plt.style.use('ggplot')
+    acc = history.history['acc']
+    val_acc = history.history['val_acc']
+    loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    x = range(1, len(acc) + 1)
+
+    plt.figure(figsize=(12, 5))
+    plt.subplot(1, 2, 1)
+    plt.plot(x, acc, 'b', label='Training acc')
+    plt.plot(x, val_acc, 'r', label='Validation acc')
+    plt.title('Training and validation accuracy')
+    plt.legend()
+    plt.subplot(1, 2, 2)
+    plt.plot(x, loss, 'b', label='Training loss')
+    plt.plot(x, val_loss, 'r', label='Validation loss')
+    plt.title('Training and validation loss')
+    plt.legend()
